fixed ef2gcn normalization
the EF2GCN layers now can't rely on feature information attached to impossible edges (those exist in the model since batches with varying graph sizes are represented via rectangular dense tensors).

implementation/src/ltag/layers.py
@@ -90,7 +90,7 @@ class EF2GCNLayer(keras.layers.Layer):
     return base_config
 
   def build(self, input_shape):
-    X_shape, n_shape = input_shape
+    X_shape, mask_shape, n_shape = input_shape
     edge_dim = X_shape[-1]
 
     self.W = self.add_weight(
@@ -104,14 +104,13 @@ class EF2GCNLayer(keras.layers.Layer):
       trainable=True, initializer=tf.initializers.Zeros)
 
   def call(self, input):
-    X, n = input
+    X, mask, n = input
 
-    X_prop = ops.edge_feature_aggregation(X, tf.multiply)
+    X_prop = ops.aggregate_edge_features(X, tf.multiply)
 
     XW = tf.linalg.matmul(X, self.W)
     XW_prop = tf.linalg.matmul(X_prop, self.W_prop)
     XW_comb = tf.nn.bias_add(XW + XW_prop, self.W_bias)
-    XW_filtered = XW_comb
-    X_out = self.act(XW_filtered)
+    X_out = self.act(XW_comb) * mask
 
-    return X_out, n
+    return X_out, mask, n

implementation/src/ltag/main.py
@@ -40,6 +40,6 @@ def train(label=None):
 
 
 # -%% codecell
-train("fiedler_dataset_wl2")
+train("triangle_dataset_wl2")
 
 model.predict(ds), np.array(list(map(lambda x: x[1].numpy(), list(ds_raw))))

implementation/src/ltag/models.py
@@ -6,6 +6,7 @@ from tensorflow import keras
 
 import ltag.chaining.pipeline as cp
 import ltag.chaining.model as cm
+import ltag.ops as ops
 from ltag.layers import EFGCNLayer, EF2GCNLayer
 
 @cm.model_inputs
@@ -14,7 +15,7 @@ def gcn_inputs(layer_dims):
   A = keras.Input(shape=(None, None), name="A")
   n = keras.Input(shape=(), dtype=tf.int32, name="n")
 
-  return (X, A, n)
+  return X, A, n
 
 @cm.model_step
 def with_layers(inputs, layer, layer_dims, **kwargs):
@@ -38,7 +39,11 @@ def with_edge_featured_propagation(inputs):
 
   AX_e = tf.concat([A_e, X_e], axis=-1)
 
-  return AX_e, n
+  X_shape = tf.shape(X)
+  max_n = X_shape[-2]
+  mask = ops.matrix_mask.python_function(n, max_n)
+
+  return AX_e, mask, n
 
 @cm.model_step
 def select_features(inputs):
@@ -50,10 +55,9 @@ def avg_verts(io):
 
   Y_shape = tf.shape(Y)
   max_n = Y_shape[-2]
-  n_mask = tf.broadcast_to(
-    tf.expand_dims(tf.sequence_mask(n, maxlen=max_n), 2), Y_shape)
+  n_mask = ops.vec_mask.python_function(n, max_n)
 
-  Y = tf.where(n_mask, Y, tf.zeros_like(Y))
+  Y = Y * n_mask
 
   y = tf.transpose(
     tf.math.divide_no_nan(
@@ -65,17 +69,6 @@ def avg_verts(io):
 @cp.pipeline_step
 def avg_edges(io):
   (X, A, n), Y = io
-
-  Y_shape = tf.shape(Y)
-  max_n = Y_shape[-2]
-  n_mask = tf.cast(tf.sequence_mask(n, maxlen=max_n), tf.int32)
-  m_1 = tf.expand_dims(n_mask, 2)
-  m_2 = tf.expand_dims(n_mask, 1)
-  m = tf.broadcast_to(
-    tf.cast(tf.expand_dims(tf.linalg.matmul(m_1, m_2), -1), tf.float32),
-    Y_shape)
-
-  Y = Y * m
 
   y = tf.transpose(
     tf.math.divide_no_nan(
@@ -99,4 +92,4 @@ EF2GCN = gcn_model("EF2GCN", [
 # Averaging GCNs:
 
 AVG_EFGCN = EFGCN.extend("AVG_EFGCN", [avg_verts])
-AVG_EF2GCN = EF2GCN.extend("EF2GCN", [avg_edges])
+AVG_EF2GCN = EF2GCN.extend("AVG_EF2GCN", [avg_edges])

implementation/src/ltag/ops.py
@@ -17,7 +17,7 @@ def normalize_mat(M):
 
 
 @tf.function
-def edge_feature_aggregation(X, agg):
+def aggregate_edge_features(X, agg):
   X_shape = tf.shape(X)
   n = X_shape[-2]
 
@@ -30,3 +30,18 @@ def edge_feature_aggregation(X, agg):
   X_prod = agg(X_1, X_2)
 
   return tf.reduce_sum(X_prod, axis=-2)
+
+@tf.function
+def vec_mask(n, max_n):
+  vec_mask = tf.cast(tf.sequence_mask(n, maxlen=max_n), tf.int32)
+
+  return tf.expand_dims(vec_mask, -1)
+
+@tf.function
+def matrix_mask(n, max_n):
+  vec_mask = tf.cast(tf.sequence_mask(n, maxlen=max_n), tf.int32)
+  m_1 = tf.expand_dims(vec_mask, 2)
+  m_2 = tf.expand_dims(vec_mask, 1)
+  m = tf.cast(tf.expand_dims(tf.linalg.matmul(m_1, m_2), -1), tf.float32)
+
+  return m