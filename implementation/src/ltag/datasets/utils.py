bcbbb66e627cd0b47fbc96c95580f354568d70b9
Clemens Damke <admin@cortys.de>
2019-12-30 23:02:31 +0100

    implemented sparse EF2GCN compute encoding
    this encoding sparsely encodes edge featured graphs and can be used as input to a static compute graph for 2-WL iterations.
    
    the required indexes are precomputed for each edge, therefore no indexing has to be done in the compute graph, only static lookups have to be performed.

--- implementation/src/ltag/datasets/utils.py
+++ implementation/src/ltag/datasets/utils.py
@@ -1,11 +1,16 @@
 from __future__ import absolute_import, division, print_function,\
   unicode_literals
 
+import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import networkx as nx
+import funcy as fy
 
-def to_tf(x, adjs, y, ragged=False, sparse=False):
+import ltag.chaining.pipeline as cp
+
+@cp.tolerant
+def to_vert_ds(x, adjs, y, ragged=False, sparse=False):
   x_in = x
   x = tf.ragged.constant(x)
   adjs = tf.ragged.constant(adjs)
@@ -27,9 +32,78 @@ def to_tf(x, adjs, y, ragged=False, sparse=False):
     tf.constant(y, dtype=tf.float32)
   ))
 
+def eid_lookup(e_ids, g, i, j):
+  if i > j:
+    i, j = j, i
+
+  return e_ids[(g, i, j)]
+
+@cp.tolerant
+def to_edge_ds(xs, adjs, ys, shuffle=False):
+  def gen():
+    il = np.arange(len(xs))
+
+    if shuffle:
+      np.random.shuffle(il)
+
+    for i in il:
+      x = xs[i]
+      y = ys[i]
+      (n, f) = x.shape
+
+      if n == 0:
+        continue
+
+      adj = adjs[i]
+      y = ys[i]
+      g = nx.from_numpy_array(adj)
+      e_zero = np.zeros(f)
+      e_ids = {}
+      x_e = []
+      ref_v = []
+      ref_u = []
+
+      for node in nx.nodes(g):
+        g.add_edge(node, node)
+
+      e_count = 0
+      for v, u in nx.edges(g):
+        e_ids[(i, v, u)] = e_count
+        e_count += 1
+
+      for edge in nx.edges(g):
+        v, u = edge
+        n = list(nx.common_neighbors(g, v, u))
+        n_v = [eid_lookup(e_ids, i, v, k) for k in n]
+        n_u = [eid_lookup(e_ids, i, u, k) for k in n]
+
+        x_e.append(x[v] if v == u else e_zero)
+        ref_v.append(n_v)
+        ref_u.append(n_u)
+
+      max_ref = np.max([len(r) for r in ref_v])
+
+      ref_v = [
+        np.pad(r, (0, max_ref - len(r)), "constant", constant_values=-1)
+        for r in ref_v]
+      ref_u = [
+        np.pad(r, (0, max_ref - len(r)), "constant", constant_values=-1)
+        for r in ref_u]
+
+      yield np.array(x_e), np.array(ref_v), np.array(ref_u), y
+
+  return tf.data.Dataset.from_generator(
+    gen,
+    output_types=(tf.float32, tf.int32, tf.int32, tf.float32))
+
 def tf_dataset_generator(f):
-  def w(*args, ragged=False, sparse=False):
-    return to_tf(*f(*args), ragged=ragged, sparse=sparse)
+  @fy.wraps(f)
+  def w(*args, edge_featured=False, **kwargs):
+    r = cp.tolerant(f)(*args, **kwargs)
+
+    return (
+      to_edge_ds(*r, **kwargs) if edge_featured
+      else to_vert_ds(*r, **kwargs))
 
   return w
 
