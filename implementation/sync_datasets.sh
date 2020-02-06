c2b5de8df2726e2f5ba389e4bc3b70bac11f037a
Clemens Damke <admin@cortys.de>
2020-02-06 21:44:23 +0100

    added synthetic dataset syncing
    preparation for the evaluation of synthetic datasets across multiple graph classifier implementations
    
    synthetic datasets are also now tracked to keep evaluation results reproducible even if the generator code changes

--- implementation/sync_datasets.sh
+++ implementation/sync_datasets.sh
@@ -4,31 +4,62 @@ cd "${BASH_SOURCE%/*}" || exit
 
 FROM_NAME=${1:-"splits"}
 
+function sync_datasets() {
+	local FROM=$1
+	local TARGETS=$2
+	local PATTERN=$3
+	local MATCH_NAMES=$(ls -d $FROM/$PATTERN)
+
+	local from_match
+	local to_match
+	local target
+
+	echo "Found $(wc -l <<< "$MATCH_NAMES") matches for $FROM/$PATTERN."
+	echo "Copying to $TARGETS."
+	for from_match in $MATCH_NAMES; do
+		for target in $TARGETS; do
+			to_match=$(sed "s|$FROM|$target|g" <<< "$from_match")
+			[ -e $to_match ] && rm -r $to_match
+			mkdir -p $(dirname $to_match)
+			cp -RT --remove-destination $from_match $to_match
+			echo "Synced: $from_match -> $to_match"
+		done
+	done
+}
+
 if [ "$FROM_NAME" == "main" ]; then
-	FROM=./data/tu
-	TARGETS="./ds_splits ./libs/gnn-comparison/DATA"
+	TU_FROM="./data/tu"
+	SYN_FROM="./data/synthetic"
+	TU_TARGETS="./ds_splits/tu ./libs/gnn-comparison/DATA"
+	SYN_TARGETS="./ds_splits/synthetic ./libs/gnn-comparison/SYN_DATA"
 elif [ "$FROM_NAME" == "comp" ]; then
-	FROM=./libs/gnn-comparison/DATA
-	TARGETS="./ds_splits ./data/tu"
+	TU_FROM="./libs/gnn-comparison/DATA"
+	SYN_FROM="./libs/gnn-comparison/SYN_DATA"
+	TU_TARGETS="./ds_splits/tu ./data/tu"
+	SYN_TARGETS="./ds_splits/synthetic ./data/synthetic"
 elif [ "$FROM_NAME" == "splits" ]; then
-	FROM=./ds_splits
-	TARGETS="./data/tu ./libs/gnn-comparison/DATA"
+	TU_FROM="./ds_splits/tu"
+	SYN_FROM="./ds_splits/synthetic"
+	TU_TARGETS="./data/tu ./libs/gnn-comparison/DATA"
+	SYN_TARGETS="./data/synthetic ./libs/gnn-comparison/SYN_DATA"
 else
 	echo "Unknown dataset location name '$FROM_NAME'."
 	echo "Use either 'main', 'comp' or 'splits' (default)."
 	exit 1
 fi
 
-SPLIT_NAMES=$(ls $FROM/*/processed/*_splits.json)
-echo "Found $(wc -l <<< "$SPLIT_NAMES") splits in $FROM."
-echo "Copying to $TARGETS."
-for from_split in $SPLIT_NAMES; do
-	for target in $TARGETS; do
-		to_split=$(sed "s#$FROM#$target#g" <<< "$from_split")
-		mkdir -p $(dirname $to_split)
-		cp $from_split $to_split
-		echo "Synced: $from_split -> $to_split"
-	done
-done
+echo "Syncing tu dataset splits..."
+# sync_datasets "$TU_FROM" "$TU_TARGETS" "*/processed/*_splits.json"
+echo "Synced tu dataset splits."
+echo
+
+echo "Syncing synthetic dataset splits..."
+# sync_datasets "$SYN_FROM" "$SYN_TARGETS" "*/processed/*_splits.json"
+echo "Synced synthetic dataset splits."
+echo
+echo "Syncing synthetic dataset raw encodings..."
+# sync_datasets "$SYN_FROM" "$SYN_TARGETS" "*/raw"
+echo "Synced synthetic dataset raw encodings."
+echo
 
 echo "Done."
