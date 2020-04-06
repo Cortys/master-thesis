46268f99414602b833f772b06df5af08aa125eb0
Clemens Damke <admin@cortys.de>
2020-04-06 20:57:32 +0200

    allow running experiments on CPU (even though this is too slow to be useful for almost all dataset)

--- implementation/evaluate_datasets.sh
+++ implementation/evaluate_datasets.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 USER="-u $(id -u):$(id -g)"
+CUDA_ENV=""
+
+if [ ! -z "$CUDA_VISIBLE_DEVICES" ]; then
+	CUDA_ENV="-e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+fi
 
 if [ -z "$LTAG_CONTAINER_NAME" ]; then
 	LTAG_CONTAINER_NAME="ltag"
 fi
 
-docker exec -it $USER $(docker ps -aqf "name=^$LTAG_CONTAINER_NAME\$") python3 ./ltag/evaluate_datasets.py $@ \
+docker exec -it $USER $CUDA_ENV $(docker ps -aqf "name=^$LTAG_CONTAINER_NAME\$") python3 ./ltag/evaluate_datasets.py $@ \
 	| grep --line-buffered -vE \
 	"BaseCollectiveExecutor::StartAbort|IteratorGetNext|Shape/|Shape_[0-9]+/"
